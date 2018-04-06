cb30ab2e6c3d2b0252d251664e911e85c915525a
DanteVDrako <ahsierra@poligran.edu.co>
2018-04-06 12:47:25 -0500

    Generadas Clases Restaurant y Reserves, descrita paleta de colores en el archivo colors.

---
+++ Proyecto/app/src/main/java/Class/Reserves.java
@@ -0,0 +1,32 @@
+package Class;
+
+/**
+ * Created by ahsierra on 06/04/2018.
+ */
+
+public class Reserves {
+
+    private String date;
+    private String personAllowed;
+
+    public Reserves(String date, String personAllowed) {
+        this.date = date;
+        this.personAllowed = personAllowed;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public String getPersonAllowed() {
+        return personAllowed;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public void setPersonAllowed(String personAllowed) {
+        this.personAllowed = personAllowed;
+    }
+}
