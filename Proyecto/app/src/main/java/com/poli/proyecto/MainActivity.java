7bcd4407d3a14c1adc3e5497c3e966c161fda558
mauromara <maurolealtadhc@gmail.com>
2018-04-07 12:14:02 -0500

    Login
    Funcionalidad del login

--- Proyecto/app/src/main/java/com/poli/proyecto/MainActivity.java
+++ Proyecto/app/src/main/java/com/poli/proyecto/MainActivity.java
@@ -5,24 +5,42 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
-    private Button a;
+    private EditText user, password;
+        private Button iniciarSesion;
+        private String userName, sPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        a = (Button)findViewById(R.id.login);
-        a.setOnClickListener(this);
+        user = (EditText) findViewById(R.id.user);
+        password = (EditText) findViewById(R.id.password);
+        iniciarSesion = (Button)findViewById(R.id.login);
+        iniciarSesion.setOnClickListener(this);
     }
 
     public void onClick(View v){
-        if(v.getId() == a.getId()){
-            Intent ir = new Intent (MainActivity.this, Reserves_3_Activity.class);
-            startActivity(ir);
+        userName = user.getText().toString();
+        sPassword = password.getText().toString();
+
+        if ((userName.equals("admin")) && (sPassword.equals("123456")))
+        {
+            Intent inicio = new Intent (MainActivity.this, Map_2_Activity.class);
+            startActivity(inicio);
+        }
+
+        else{
+            Toast.makeText(iniciarSesion.getContext(),"Usuario Incorrecto",Toast.LENGTH_SHORT).show();
+        }
+
+
+
         }
     }
-}
+
