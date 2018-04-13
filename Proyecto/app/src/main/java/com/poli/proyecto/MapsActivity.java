2a0433298f0172f4893bd5b95457edfcc2d1b785
mauromara <maurolealtadhc@gmail.com>
2018-04-13 13:57:36 -0500

    Implemetación mapa
    Se incluye mapa a la APP

--- Proyecto/app/src/main/java/com/poli/proyecto/MapsActivity.java
+++ Proyecto/app/src/main/java/com/poli/proyecto/MapsActivity.java
@@ -1,26 +1,35 @@
 package com.poli.proyecto;
 
+
 import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
-public class Map_2_Activity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
+public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback,NavigationView.OnNavigationItemSelectedListener  {
     private DrawerLayout menu;
     private ActionBarDrawerToggle icon;
+    private GoogleMap mMap;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_map_2_);
-
+        setContentView(R.layout.activity_maps);
         //Funcionalidad del menu
         menu = (DrawerLayout)findViewById(R.id.draw);
         icon = new ActionBarDrawerToggle(this, menu, R.string.open, R.string.close);
@@ -30,18 +39,37 @@ public class Map_2_Activity extends AppCompatActivity implements NavigationView.
         NavigationView nv = (NavigationView)findViewById(R.id.nav);
         nv.setNavigationItemSelectedListener(this);
 
+
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
     }
 
 
 
 
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng sydney= new LatLng(4.0000000, -72.0000000);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Colombia papá"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
 
 
-
-
-
-
-
+    }
 
     //Metodos del Menu
     @Override
@@ -70,7 +98,7 @@ public class Map_2_Activity extends AppCompatActivity implements NavigationView.
     }
     //Metodos para cambiar de activity
     public void executeRestaurantList(View v){
-        Intent ejecutar = new Intent(Map_2_Activity.this, Reserves_3_Activity.class);
+        Intent ejecutar = new Intent(MapsActivity.this, Reserves_3_Activity.class);
         startActivity(ejecutar);
     }
 
